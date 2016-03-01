5cf850ec5cd4f1f0aa0b70148d76d71de803fc50
xianjunzheng.backup@gmail.com <xianjunzheng.backup@gmail.com>
2016-03-01 14:57:41 +1100

    code on Tue Mar  1 14:57:41 AEDT 2016

---
+++ properties/items.jl
@@ -0,0 +1,1 @@
+{"price": ["715.0"], "title": ["bermondsey ec kennington drive acton seven rm"], "description": ["bakerloo single they glazed sized fittings angel\r\nair cupboard high\r\nexcellent fulham offer are serviced\r\nhour universities into environment students direct gated and"], "image_urls": ["../images/i07.jpg"], "address": ["West Hampstead, London"]}
